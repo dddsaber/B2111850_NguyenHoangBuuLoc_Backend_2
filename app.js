e11b317867dc4abc4cff1a64b45a0218713c4d61
dddsaber <nhbl.loc@gmail.com>
2024-08-12 16:42:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const cors = require("cors");
 // Router
 const contactsRouter = require("./app/routes/contact.route");
 
+// Thêm các middleware xử lý lỗi
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy nếu không match route nào
+  //next() để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Define error-handling middleware last, after other app.use() and routes calls
+app.use((error, req, res, next) => {
+  // Middleware xử lý lỗi tập trung.
+  // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application!" });
