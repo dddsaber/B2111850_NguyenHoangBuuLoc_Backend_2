e3f6cda4b3b4c25f50834b9de962127e79feb622
dddsaber <nhbl.loc@gmail.com>
2024-08-21 19:21:58 +0700

    Cai dat  express chay thong bao xin chao

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const config = require("./app/config");
 
-// Start Server
+// Start Server Express
 const PORT = config.app.port;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
